771855aad98ddbbc3367e95a96c3afc726196af3
Fernando Iván Sevilla Valderrama <feseva99@gmail.com>
2021-09-27 21:02:30 +0200

    Update exercise4.kt

--- kotlinExercises/src/exercise4.kt
+++ kotlinExercises/src/exercise4.kt
@@ -5,10 +5,12 @@ fun main(){
 
     println("a and b = ${a and b}")
     //AND = 00000011 - 3
-    println("a or b = ${a or b}")
+    println("a xor b = ${a or b}")
     //OR  = 10101011 - 171
+    println("a or b = ${a xor b}")
+    //XOR  = 00101010 - 168
     println("a shl 3 = ${a shl 3}")
-    //shl = 01011000 - 88
+    //shl = 01011000 - 1368
     println("a shr 2 = ${a shr 2}")
     //shr = 00101010 - 42
 }
